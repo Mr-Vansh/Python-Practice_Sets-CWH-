2dc0b95b87d8568f09a8763efb0a6fa7ef6d1627
Vansh Chitlangia <85049151+Mr-Vansh@users.noreply.github.com>
2022-01-12 14:56:24 +0530

    Add files via upload

---
+++ Practice Set 4/04_SumOfNumbers.py
@@ -0,0 +1,4 @@
+n = [10, 12, 4, 22, 20]
+
+print("Without inbuilt function is :",n[0] + n[1] + n[2] + n[3] + n[4])
+print("With inbuilt function is :",sum(n))
---
+++ Practice Set 4/05_CountTotalTuple.py
@@ -0,0 +1,2 @@
+a = (7 , 0 , 8 , 0 , 0 , 9)
+print("Total number of 0 in the tuple is :",a.count(0))
