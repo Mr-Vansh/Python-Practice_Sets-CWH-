c69593e3087a5b970d6dbfa225ec5251c1f006ab
Vansh Chitlangia <85049151+Mr-Vansh@users.noreply.github.com>
2022-01-13 17:33:00 +0530

    Add files via upload

---
+++ Practice Set 5/06_FavLanguage.py
@@ -0,0 +1,13 @@
+favLang = {}
+
+a = input("Enter your favorite language, Vansh : ")
+b = input("Enter your favorite language, Vedant : ")
+c = input("Enter your favorite language, Paarth : ")
+d = input("Enter your favorite language, Sahil : ")
+
+favLang["Vansh"] = a
+favLang["Vedant"] = b
+favLang["Paarth"] = c
+favLang["Sahil"] = d
+
+print(favLang)
